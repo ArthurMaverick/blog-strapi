57cec54ae94d66d92635f341075ccaf01a8c77b2
ArthurMaverick <arthuracs18@gmail.com>
2021-01-20 17:50:54 -0300

    blog2

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,15 @@
 'use strict';
-
+const axios = require('axios')
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
+  },
+  async afterUpdate(result, params, data) {
+    axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
+  }
+};
