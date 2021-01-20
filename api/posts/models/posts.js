6bc40628b04720936ef66c3bb4998216be16659e
ArthurMaverick <arthuracs18@gmail.com>
2021-01-20 17:55:48 -0300

    blog2

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -6,10 +6,13 @@ const axios = require('axios')
  */
 
 module.exports = {
-  async afterCreate(result, data) {
-    axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
-  },
-  async afterUpdate(result, params, data) {
-    axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
+    },
+    async afterUpdate(result, params, data) {
+      axios.post('https://api.netlify.com/build_hooks/60085f568fb8ac444585400d')
+    }
   }
+
 };
